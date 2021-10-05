a363fae6a8e6803e15c1d571855eb29ea44296d8
Kenneth Babigumira <kenneth.babigumira@capgemini.com>
2021-10-05 17:12:20 +0100

    Updated taskId to make it unique, also updated the Pester tests

--- tests/RunTests.ps1
+++ tests/RunTests.ps1
@@ -13,16 +13,39 @@ if ($modules.Name -notcontains 'pester') {
 Import-Module Pester -MinimumVersion 5.0.0
 
 $Env:BUILD_BUILDID = "B01"
-# Get-Childitem *.Tests.ps1 -recurse | ForEach-Object { Invoke-Pester -CI -Path $_.FullName}
-# $testResults = Invoke-Pester -OutputFile Test.xml -OutputFormat NUnitXml -CodeCoverage (Get-ChildItem -Path $PSScriptRoot\*.psm1 -Exclude *.Tests.* ).FullName -PassThru
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 $path = Resolve-Path -Path "$scriptPath\..\src\extensions\tasks\owasp-scan\ps_modules"
 
-# Get-Childitem *.Tests.ps1 -recurse | ForEach-Object { Invoke-Pester -CI -Path $_.FullName -OutputFormat NUnitXml  -OutputFile "./Test-Pester.xml" -CodeCoverage "$path/$([System.IO.Path]::GetFileNameWithoutExtension($_)).psm1" -PassThru}
 Get-Childitem *.Tests.ps1 -recurse -exclude "CreateReleaseNotesTest.tests.ps1"  | ForEach-Object { 
+
     [string]$modulePath="$path\$([System.IO.Path]::GetFileNameWithoutExtension($_).Replace('.Tests', '')).psm1"; 
     Write-Host $modulePath
-    Invoke-Pester -Script $_.FullName -OutputFile "$scriptPath\TestResults\Pester-Tests.xml" -OutputFormat NUnitXml  -Verbose -CodeCoverage "$modulePath" -CodeCoverageOutputFile "$scriptPath\TestResults\Pester-Coverage.xml" -CodeCoverageOutputFileFormat JaCoCo -PassThru -Show All
+
+    $configuration = [PesterConfiguration]@{
+        Run = @{
+            Path = $_.FullName
+        }
+        Output = @{
+            Verbosity = 'Detailed'
+        }
+        TestResult   = @{
+            Enabled      = $true
+            OutputFormat = "NUnitXml"
+            OutputPath   = "$scriptPath\TestResults\Pester-Coverage.xml"
+        }
+        CodeCoverage = @{
+            Enabled      = $true
+            Path         = $modulePath
+            OutputFormat = "JaCoCo"
+            OutputPath   = "$scriptPath\TestResults\Pester-Coverage.xml"
+        }
+        Should = @{
+            ErrorAction = 'Continue'
+        }
+    }
+
+    Invoke-Pester -Configuration $Configuration
+
 }
 
